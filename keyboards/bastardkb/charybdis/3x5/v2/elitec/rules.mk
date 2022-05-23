9779fe00350dba50a19267a86e61452002f4507d
Matthias Ervens <matt@tuxship.org>
2022-05-23 22:14:25 +0200

    crkbd with rp2040
    share keymap for crkbd and cnano via userspace
    
    windows swapper + combos
    
    added build.yml
    
    moved layers to extra file
    
    moved build job
    
    use pmw3389 with cnano; new mouse layer
    
    copied pointing/split transport  template from drashna
    
    more combos + fix auto mouse layer
    
    fixed rp2040 board + more combos
    
    different image for pipeline
    
    pointing + clear combo
    
    build attributes
    
    build attributes
    
    use qmk for building
    
    use qmk for building (2)
    
    update build pipeline
    
    injecting secrets
    
    injecting secrets
    
    Conflicts:
    	keyboards/bastardkb/charybdis/3x5/v2/elitec/config.h
    	keyboards/bastardkb/charybdis/3x5/v2/elitec/rules.mk

--- keyboards/bastardkb/charybdis/3x5/v2/elitec/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/v2/elitec/rules.mk
@@ -1,4 +1,9 @@
 AUDIO_SUPPORTED = no        # Audio is not supported
 
-POINTING_DEVICE_DRIVER = pmw3360
+# Charybdis nano is a split 3x5 keyboard with a maximum of 3 thumb keys (2 on
+# the trackball side).
+SPLIT_KEYBOARD = yes  # todo mses moved in 569be5951e94c59138a172871f4213ec802e6877
+
+# POINTING_DEVICE_ENABLE = yes # Enable trackball # todo mses moved in 569be5951e94c59138a172871f4213ec802e6877, seems to be in info.json
+POINTING_DEVICE_DRIVER = pmw3389
 MOUSE_SHARED_EP = no # Unify multiple HID interfaces into a single Endpoint
