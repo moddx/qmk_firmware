7ddadb5d722da53499cf17d4cb4c7e9de1fe410a
Matthias Ervens <matt@tuxship.org>
2024-03-23 12:29:19 +0100

    custom layout etc

--- users/moddx/moddx.c
+++ users/moddx/moddx.c
@@ -57,66 +57,66 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Default, L0
-	[_DEFAULT] = LAYOUT_split_3x5_3(
-		KC_X,         KC_V,         KC_L,         KC_C,         KC_W,           KC_K,   KC_H,   KC_G,    KC_F,    KC_Q,
+	[_DEFAULT] = LAYOUT(
+		KC_X,         KC_V,          KC_L,          KC_C,          KC_W,           KC_K,   KC_H,   KC_G,    KC_F,    KC_Q,
 		LALT_T(KC_U), LT(_L4, KC_I), LT(_L5, KC_A), LT(_L3, KC_E), KC_O,           KC_S,   KC_N,   KC_R,    KC_T,    KC_D,
-		LSFT_T(DE_Y), MO(_MOUSE),   KC_LEAD,      KC_P,         DE_Z,           KC_B,   KC_M,   KC_COMM, KC_DOT,  RSFT_T(KC_J),
-		                            KC_LSFT, KC_LCTL, MO(_L1),       KC_SPC, MO(_L2), U_NP
+		LSFT_T(DE_Y), MO(_MOUSE),    QK_LEAD,       KC_P,          DE_Z,           KC_B,   KC_M,   KC_COMM, KC_DOT,  RSFT_T(KC_J),
+		                             KC_LSFT,       KC_LCTL,       MO(_L1),        KC_SPC, MO(_L2)
     ),
 
-    [_HL] = LAYOUT_split_3x5_3(
+    [_HL] = LAYOUT(
 		KC_TAB,  KC_Q, KC_W, KC_E, KC_R,           KC_Z,   KC_U,   KC_I,    KC_O,    KC_P,
-		KC_LSFT, KC_A, KC_S, KC_D, KC_F,           KC_H,   KC_J,   KC_K,    KC_L,    KC_LEAD,
+		KC_LSFT, KC_A, KC_S, KC_D, KC_F,           KC_H,   KC_J,   KC_K,    KC_L,    QK_LEAD,
 		KC_LCTL, DE_Y, DE_5, KC_4, KC_3,           KC_N,   KC_M,   KC_COMM, KC_DOT,  RSFT_T(DE_MINS),
-		               KC_2, KC_1, KC_SPC,         KC_ESC, TG(_HL), U_NP
+		               KC_2, KC_1, KC_SPC,         KC_ESC, TG(_HL)
     ),
 
     // _L1
-	[_L1] = LAYOUT_split_3x5_3(
+	[_L1] = LAYOUT(
         KC_TAB,  KC_ESC,     KC_ENT,     LCTL(DE_Z), KC_INS,        DM_REC1, DE_HASH,       DE_LCBR, DE_RCBR, KC_MPLY,
 		KC_LCTL, KC_SPC,     LCTL(KC_S), KC_HOME,    KC_END,        KC_LEFT, KC_DOWN,       KC_UP,   KC_RGHT, DE_LPRN,
 		KC_LSFT, LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_NO,         KC_DEL,  KC_BSPC,       DE_LBRC, DE_RBRC, DE_RPRN,
-		                     KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_LALT, OSM(MOD_LGUI), U_NP
+		                     KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_LALT, OSM(MOD_LGUI)
     ),
 
 	// _L2
-	[_L2] = LAYOUT_split_3x5_3(
+	[_L2] = LAYOUT(
 		EMAIL,          DE_BSLS,     DE_EQL,  DE_AMPR, DE_TILD,       DM_PLY1, LSFT(KC_F3), DE_ASTR, KC_F3,   DE_PLUS,
 		LCTL_T(DE_AT),  DE_LABK,     DE_RABK, DE_MINS, DE_PIPE,       MC_SWLE, KC_PGDN,     KC_PGUP, MC_SWRI, KC_TRNS,
 		LSFT_T(KC_APP), LALT(KC_F4), KC_NO,   KC_NO,   KC_NO,         DE_SS,   DE_UDIA,     DE_ODIA, DE_ADIA, KC_RSFT,
-		                             KC_TRNS, KC_CAPS, KC_TRNS,       KC_TRNS, KC_TRNS,     U_NP
+		                             KC_TRNS, KC_CAPS, KC_TRNS,       KC_TRNS, KC_TRNS
     ),
 
 	// _L3 NUM
-	[_L3] = LAYOUT_split_3x5_3(
+	[_L3] = LAYOUT(
 		TG(_HL),       KC_TRNS,       EH_RGHT,       EH_LEFT,       QK_BOOT,              DE_SLSH, KC_7,   KC_8,    KC_9,  DE_PLUS,
-		OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LCTL), KC_TRNS,       EEP_RST,              DE_ASTR, KC_4,   KC_5,    KC_6,  DE_MINS,
+		OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LCTL), KC_TRNS,       EE_CLR,              DE_ASTR, KC_4,   KC_5,    KC_6,  DE_MINS,
 		OSM(MOD_LSFT), KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,              KC_BSPC, KC_1,   KC_2,    KC_3,  KC_RSFT,
-                                      OSM(MOD_LGUI), OSM(MOD_LCTL), KC_TRNS,              KC_0,    KC_DOT, U_NP
+                                      OSM(MOD_LGUI), OSM(MOD_LCTL), KC_TRNS,              KC_0,    KC_DOT
     ),
 
     // _L4 NUM SYMS
-	[_L4] = LAYOUT_split_3x5_3(
+	[_L4] = LAYOUT(
 		KC_TRNS,       KC_TRNS,  KC_TRNS,       KC_TRNS,       KC_TRNS,              KC_NO,      LSFT(KC_7),  LSFT(KC_8),  LSFT(KC_9), KC_NO,
 		OSM(MOD_LALT), KC_TRNS,  OSM(MOD_LCTL), MO(_L5),       KC_TRNS,              KC_NO,      LSFT(KC_4),  LSFT(KC_5),  LSFT(KC_6), KC_NO,
 		OSM(MOD_LSFT), KC_TRNS,  KC_TRNS,       KC_TRNS,       KC_TRNS,              KC_BSPC,    LSFT(KC_1),  LSFT(KC_2),  LSFT(KC_3), KC_NO,
-		                         OSM(MOD_LGUI), OSM(MOD_LCTL), KC_TRNS,              LSFT(KC_0), KC_DOT, U_NP
+		                         OSM(MOD_LGUI), OSM(MOD_LCTL), KC_TRNS,              LSFT(KC_0), KC_DOT
     ),
 
 	// _L5 F-Keys
-	[_L5] = LAYOUT_split_3x5_3(
+	[_L5] = LAYOUT(
 		KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,          KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,
 		OSM(MOD_LALT), OSM(MOD_LSFT), KC_TRNS,       KC_TRNS,       KC_TRNS,          KC_TRNS, KC_F4,   KC_F5,   KC_F6,   KC_F11,
 		OSM(MOD_LSFT), KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,          KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F12,
-		                              OSM(MOD_LGUI), OSM(MOD_LCTL), KC_TRNS,          KC_TRNS, KC_TRNS, U_NP
+		                              OSM(MOD_LGUI), OSM(MOD_LCTL), KC_TRNS,          KC_TRNS, KC_TRNS
     ),
 
-    // L5 F-Keys
-	[_MOUSE] = LAYOUT_split_3x5_3(
-		KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_ACCEL,         KC_NO, KC_NO,   DPI_RMOD,  DPI_MOD, KC_NO,
-		OSM(MOD_LALT), OSM(MOD_LSFT), KC_BTN1,       KC_BTN2,       KC_TRNS,          KC_NO, KC_BTN1, KC_BTN3,   KC_BTN2, KC_NO,
-		OSM(MOD_LSFT), KC_TRNS,       SNIPING,       DRGSCRL,       KC_TRNS,          KC_NO, DRGSCRL, KC_NO,     KC_NO,   KC_NO,
-                                      OSM(MOD_LSFT), OSM(MOD_LCTL), MO(_L1),          KC_NO, KC_NO, U_NP
+    // _MOUSE
+	[_MOUSE] = LAYOUT(
+		KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_ACCEL,         KC_NO,   KC_NO,   DPI_RMOD,  DPI_MOD, KC_NO,
+		KC_LALT,       KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_NO,            KC_NO,   KC_BTN1, KC_BTN3,   KC_BTN2, KC_NO,
+		KC_LSFT,       KC_TRNS,       SNIPING,       DRGSCRL,       KC_NO,            KC_NO,   DRGSCRL, KC_NO,     KC_NO,   KC_NO,
+                                      KC_LSFT,       KC_LCTL,       MO(_L1),          KC_LALT, KC_TRNS
     )
 
 };
@@ -167,38 +167,40 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+        case LSFT_T(DE_Y):
+        case RSFT_T(KC_J):
+            // Do select the hold action when another key is tapped.
+            return true;
         default:
             // Do not select the hold action when another key is tapped.
             return false;
     }
 }
 
-//LEADER_EXTERNS();
 
-// void matrix_scan_user(void) {
-//   LEADER_DICTIONARY() {
-//     leading = false;
-//     leader_end();
+void leader_start_user(void) {
+    // Do something when the leader key is pressed
+}
 
-//     SEQ_ONE_KEY(KC_S) {
-//       tap_code16(DE_SS);
-//     }
+void leader_end_user(void) {
+    if (leader_sequence_one_key(KC_S)) {
+      tap_code16(DE_SS);
+    }
 
-//     SEQ_ONE_KEY(KC_Q) {
-//       tap_code16(S(DE_SS));
-//     }
+    if (leader_sequence_one_key(KC_Q)) {
+      tap_code16(S(DE_SS));
+    }
 
-//     SEQ_ONE_KEY(KC_E) {
-//       tap_code16(DE_EURO);
-//     }
+    if (leader_sequence_one_key(KC_E)) {
+      tap_code16(DE_EURO);
+    }
 
-//     SEQ_ONE_KEY(KC_P) {
-//       tap_code16(C(S(A(KC_Q))));
-//     }
+    if (leader_sequence_one_key(KC_P)) {
+      tap_code16(C(S(A(KC_Q))));
+    }
 
-//     SEQ_ONE_KEY(KC_N) {
-//       SEND_STRING(SECRET_JENKINS);
-//       tap_code(KC_ENTER);
-//     }
-//   }
-// }
+    if (leader_sequence_one_key(KC_N)) {
+      SEND_STRING(SECRET_JENKINS);
+      tap_code(KC_ENTER);
+    }
+}
