206046967fb6039d0ec42713fefb2df78934a4a5
Matthias Ervens <matt@tuxship.org>
2024-07-18 00:29:34 +0200

    making charybdis work again + few layout/combo changes

--- keyboards/bastardkb/charybdis/3x5/3x5.h
+++ keyboards/bastardkb/charybdis/3x5/3x5.h
@@ -1,49 +1,49 @@
-/*
- * Copyright 2021 Quentin LEBASTARD <qlebastard@gmail.com>
- * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// /*
+//  * Copyright 2021 Quentin LEBASTARD <qlebastard@gmail.com>
+//  * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
+//  *
+//  * This program is free software: you can redistribute it and/or modify
+//  * it under the terms of the GNU General Public License as published by
+//  * the Free Software Foundation, either version 2 of the License, or
+//  * (at your option) any later version.
+//  *
+//  * This program is distributed in the hope that it will be useful,
+//  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  * GNU General Public License for more details.
+//  *
+//  * You should have received a copy of the GNU General Public License
+//  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  */
 
-#pragma once
+// #pragma once
 
-#include "charybdis.h"
+// #include "charybdis.h"
 
-/**
- * \brief Compatibility layout with the split_3x5_3 community layout.
- *
- * This effectively renders the Charbdis Nano compatible with existing layout
- * implementations relying on the `split_3x5_3` layout, which includes, among
- * others, Manna-Harbour's (@manna_harbour) Miryoku layout.
- *
- * The last key on the layout is ignored, to transform the input 36-keys layout
- * into the Charybdis Nano's 35-keys layout.
- */
-#define LAYOUT( \
-	k00, k01, k02, k03, k04,     			k44, k43, k42, k41, k40, \
-	k10, k11, k12, k13, k14,	    		k54, k53, k52, k51, k50, \
-	k20, k21, k22, k23, k24,    			k64, k63, k62, k61, k60, \
-		      k32, k33, k34,		        k74, k73  \
-)\
-{\
-	{ k00, k01, k02, k03, k04 }, \
-	{ k10, k11, k12, k13, k14 }, \
-	{ k20, k21, k22, k23, k24 }, \
-	{ KC_NO, KC_NO, k32, k33, k34 }, \
-	{ k40, k41, k42, k43, k44 }, \
-	{ k50, k51, k52, k53, k54 }, \
-	{ k60, k61, k62, k63, k64 }, \
-	{ KC_NO, KC_NO, KC_NO, k73, k74 }, \
-}
-// clang-format on
+// /**
+//  * \brief Compatibility layout with the split_3x5_3 community layout.
+//  *
+//  * This effectively renders the Charbdis Nano compatible with existing layout
+//  * implementations relying on the `split_3x5_3` layout, which includes, among
+//  * others, Manna-Harbour's (@manna_harbour) Miryoku layout.
+//  *
+//  * The last key on the layout is ignored, to transform the input 36-keys layout
+//  * into the Charybdis Nano's 35-keys layout.
+//  */
+// #define LAYOUT( \
+// 	k00, k01, k02, k03, k04,     			k44, k43, k42, k41, k40, \
+// 	k10, k11, k12, k13, k14,	    		k54, k53, k52, k51, k50, \
+// 	k20, k21, k22, k23, k24,    			k64, k63, k62, k61, k60, \
+// 		      k32, k33, k34,		        k74, k73  \
+// )\
+// {\
+// 	{ k00, k01, k02, k03, k04 }, \
+// 	{ k10, k11, k12, k13, k14 }, \
+// 	{ k20, k21, k22, k23, k24 }, \
+// 	{ KC_NO, KC_NO, k32, k33, k34 }, \
+// 	{ k40, k41, k42, k43, k44 }, \
+// 	{ k50, k51, k52, k53, k54 }, \
+// 	{ k60, k61, k62, k63, k64 }, \
+// 	{ KC_NO, KC_NO, KC_NO, k73, k74 }, \
+// }
+// // clang-format on
